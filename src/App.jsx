b539567ee3ccf9c0ab0830c86079779d2bbc8a8a
cootanasy <namkhue2606@gmail.com>
2023-12-11 09:24:51 +0700

    update add-baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,126 @@
-import Button from '@mui/material/Button';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import NavigationIcon from '@mui/icons-material/Navigation';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
-function App() {
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+
+import { useColorScheme } from '@mui/material/styles';
+
+// import ToggleButton from '@mui/material/ToggleButton';
+// import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+
+// function ColorToggleButton() {
+//   const { mode, setMode } = useColorScheme();
+
+//   const handleChange = (event) => {
+//     setMode(event.target.value);
+//   };
+
+//   return (
+    
+//       <ToggleButtonGroup
+//         color="primary"
+//         value={mode}
+//         exclusive
+//         onChange={handleChange}
+//       >
+//         <ToggleButton value="light">
+//           <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+//             <LightModeIcon fontSize='small' /> Light
+//           </Box>
+//         </ToggleButton>
+//         <ToggleButton value="dark">
+//           <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+//             <DarkModeIcon fontSize='small' /> Dark
+//           </Box>
+//         </ToggleButton>
+//         <ToggleButton value="system">
+//           <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+//             <SettingsBrightnessIcon fontSize='small' /> System
+//           </Box>
+//         </ToggleButton>
+//       </ToggleButtonGroup>
+    
+//   );
+// }
+
+function SwitchLightDarkMode() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    setMode(event.target.value);
+  };
 
   return (
-    <>
-      <div>asdas</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-light-dark-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-light-dark-mode"
+        id="select-light-dark-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize='small' /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize='small' /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
-      <br />
-      
-      <Fab color="primary" aria-label="add">
-        <AddIcon />
-      </Fab>
-      <Fab color="secondary" aria-label="edit">
-        <EditIcon />
-      </Fab>
-      <Fab variant="extended">
-        <NavigationIcon sx={{ mr: 1 }} />
-        Navigate
-      </Fab>
-      <Fab disabled aria-label="like">
-        <FavoriteIcon />
-      </Fab>
-    </>
+function App() {
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
+      {/* <ColorToggleButton /> */}
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trelloCustom.appBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+        <SwitchLightDarkMode />
+      </Box>
+
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: (theme) => theme.trelloCustom.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+        Board bar
+      </Box>
+        
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width: '100%',
+        height: (theme) => `calc(100vh - (${theme.trelloCustom.appBarHeight} + ${theme.trelloCustom.boardBarHeight}))`,
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+
+      </Box>
+    </Container>
   )
 }
 
