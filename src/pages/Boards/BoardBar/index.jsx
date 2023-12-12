57d07b0213f0e073607b49ae9bedf00acbfcf610
cootanasy <namkhue2606@gmail.com>
2023-12-12 11:22:47 +0700

    trello appbar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,16 +1,110 @@
 import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import LockPersonIcon from '@mui/icons-material/LockPerson';
+import AddToDriveIcon from '@mui/icons-material/AddToDrive';
+import BoltIcon from '@mui/icons-material/Bolt';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
+import { Tooltip } from '@mui/material';
+import Button from '@mui/material/Button';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+
+const MENU_STYLES = {
+  color: 'primary.main',
+  // bgcolor: 'primary',
+  border: 'none',
+  paddingX: 0.5,
+  borderRadius: 0.5,
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main',
+  },
+  '&:hover': {
+    // bgcolor: 'primary.50',
+  },
+};
 
 function BoardBar() {
   return (
     <Box sx={{
-        backgroundColor: 'primary.dark',
-        width: '100%',
-        height: (theme) => theme.trelloCustom.boardBarHeight,
-        display: 'flex',
-        alignItems: 'center',
-      }}>
-        Board bar
+      width: '100%',
+      height: (theme) => theme.trelloCustom.boardBarHeight,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 2,
+      paddingX: 2,
+      overflowX: 'auto',
+      borderTop: '1px solid #00bfa5'
+    }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <Chip 
+          sx={MENU_STYLES} 
+          icon={<DashboardIcon />} 
+          label="cootanasy" 
+          onClick={() => {}}
+        />
+        <Chip 
+          sx={MENU_STYLES} 
+          icon={<LockPersonIcon />} 
+          label="Public/Private workspace" 
+          onClick={() => {}}
+        />
+        <Chip 
+          sx={MENU_STYLES} 
+          icon={<AddToDriveIcon />} 
+          label="Add to google drive" 
+          onClick={() => {}}
+        />
+        <Chip 
+          sx={MENU_STYLES} 
+          icon={<BoltIcon />} 
+          label="Automation" 
+          onClick={() => {}}
+        />
+        <Chip 
+          sx={MENU_STYLES} 
+          icon={<FilterListIcon />} 
+          label="Filters" 
+          onClick={() => {}}
+        />
       </Box>
+      
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <Button variant="outlined">
+          <PersonAddAltIcon sx={{ mr: 1 }} />
+          Invite
+        </Button>
+
+        <AvatarGroup 
+          max={4}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16,
+            }
+          }}
+        >
+          <Tooltip title="cootanasy">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+          <Tooltip title="cootanasy">
+            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          </Tooltip>
+          <Tooltip title="cootanasy">
+            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+          </Tooltip>
+          <Tooltip title="cootanasy">
+            <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+          </Tooltip>
+          <Tooltip title="cootanasy">
+            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
+    </Box>
   )
 }
 
--- src/theme.js
+++ src/theme.js
@@ -3,8 +3,8 @@ import { experimental_extendTheme as extendTheme} from '@mui/material/styles';
 
 const theme = extendTheme({
   trelloCustom: {
-    appBarHeight: '64px',
-    boardBarHeight: '58px',
+    appBarHeight: '58px',
+    boardBarHeight: '60px',
   },
   colorSchemes: {
     light: {
@@ -17,6 +17,56 @@ const theme = extendTheme({
       palette: {
         primary: blueGrey,
         secondary: orange,
+      },
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px',
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: '#bdc3c7',
+            borderRadius: '8px',
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#00b894',
+          },
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem',
+        }),
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem',
+          '.MuiOutlinedInput-notchedOutline': {
+            borderColor: theme.palette.primary.light,
+          },
+          '&:hover': {
+            '.MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.primary.main,
+            },
+          },
+        }),
       },
     },
   },
